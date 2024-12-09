d1ea4c336014d067038040c30103645d8eae5b66
Erik Persson <perssonerik7@gmail.com>
2024-12-09 13:19:40 +0100

    changed variablen ame

--- index.js
+++ index.js
@@ -3,11 +3,11 @@ require("dotenv").config();
 const express = require("express"); // Import express, a light-weight framework
 const app = express(); // Init express, and save it in "app" variable
 const mongoose = require("mongoose"); // Import mongoose, a tool that gives NoSQL DB (such as Mongodb), the ablilities of a relational DB (such MySQL)
-const corse = require("cors");
+const cors = require("cors");
 const helmet = require("helmet");
 
 //Middleware
-app.use(corse()); // Allow cross-origin requests
+app.use(cors()); // Allow cross-origin requests
 app.use(helmet()); // Protection. Needs explanation
 app.use(express.json()); // Formats data to Json
 
