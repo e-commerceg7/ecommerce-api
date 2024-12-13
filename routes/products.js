0c450f18d2e31b7016351dc7db344efec24cf009
Erik Persson <perssonerik7@gmail.com>
2024-12-13 15:11:27 +0100

    all endpoints done

--- routes/products.js
+++ routes/products.js
@@ -3,12 +3,30 @@ const router = express.Router();
 const Product = require("../models/Product");
 
 // GET all products
+//npm start
+//http://localhost:5001/products
 router.get("/", async (req, res) => {
   try {
     console.log(Product);
     res.json(await Product.find());
   } catch (error) {
     res.json({ message: error });
+  }
+});
+
+//GET specific product by id
+// npm start
+// http://localhost:5001/products/675874568c163b7fe8d0b275
+router.get("/:id", async (req, res) => {
+  const { id } = req.params;
+  try {
+    const product = await Product.findById(id);
+    if (!product) {
+      return res.status(404).json({ message: "Product not found" });
+    }
+    res.json(product);
+  } catch (error) {
+    res.status(500).json({ message: "Error fetching product", error });
   }
 });
 
@@ -39,6 +57,58 @@ router.post("/", async (req, res) => {
       message: "Error creating product",
       error: error.message,
     });
+  }
+});
+
+//update a product by id
+router.put("/:id", async (req, res) => {
+  const { id } = req.params; // Hämta id från URL-parameter
+  const { name, description, price, color, stock, categories, image } =
+    req.body; // Hämta nya data från request body
+
+  try {
+    // Försök att hitta produkten och uppdatera den med de nya värdena
+    const updatedProduct = await Product.findByIdAndUpdate(
+      id, // Hitta produkt med detta id
+      {
+        name,
+        description,
+        price,
+        color,
+        stock,
+        categories,
+        image,
+      },
+      { new: true } // Returnera den uppdaterade produkten istället för den gamla
+    );
+
+    if (!updatedProduct) {
+      return res.status(404).json({ message: "Product not found" });
+    }
+
+    // Returnera den uppdaterade produkten
+    res.json(updatedProduct);
+  } catch (error) {
+    res.status(500).json({ message: "Error updating product", error });
+  }
+});
+
+//delete a product
+router.delete("/:id", async (req, res) => {
+  const { id } = req.params;
+
+  try {
+    // Försök att hitta och ta bort produkten med det givna id:t
+    const deletedProduct = await Product.findByIdAndDelete(id);
+
+    if (!deletedProduct) {
+      return res.status(404).json({ message: "Product not found" });
+    }
+
+    // Returnera ett meddelande om att produkten har raderats
+    res.json({ message: "Product deleted successfully" });
+  } catch (error) {
+    res.status(500).json({ message: "Error deleting product", error });
   }
 });
 
